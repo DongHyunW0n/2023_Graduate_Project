4a42c3073915264ebaabd5058889db87228069ce
DongHyunW0n <68365742+DongHyunW0n@users.noreply.github.com>
2023-04-04 02:11:28 +0900

    지도 살짝 구현

--- 2023_Graduate_Project/MapViewController.swift
+++ 2023_Graduate_Project/MapViewController.swift
@@ -6,24 +6,70 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, MKMapViewDelegate {
+    
+    let map = MKMapView()
+    let coordinate = CLLocationCoordinate2D(
+        latitude: 35.19716238064634,
+        longitude: 129.06314400423676)
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        view.addSubview(map)
+        map.frame = view.bounds
+        
+        map.setRegion(MKCoordinateRegion(center: coordinate,
+                                         span: MKCoordinateSpan(
+                                            latitudeDelta: 0.1,
+                                            longitudeDelta: 0.1
+                                         )
+                                        ),
+                      animated: false)
+        
+        map.delegate = self
+        addCustomPin()
+        
+    }
 
-        // Do any additional setup after loading the view.
+    private func addCustomPin(){
+        
+        let pin = MKPointAnnotation()
+        pin.coordinate = coordinate
+        pin.title = "우리집"
+        pin.subtitle = "사직자이"
+        map.addAnnotation(pin)
+        
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard !(annotation is MKUserLocation) else{
+            return nil
+        }
+        
+        var annotationView = map.dequeueReusableAnnotationView(withIdentifier: "custom")
+        
+        if annotationView == nil{
+            
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "custom")
+            annotationView?.canShowCallout = true
+            
+            //create the view
+        }
+        else{
+            annotationView?.annotation = annotation
+        }
+        
+        
+        annotationView?.image = UIImage(named: "mapMarker")
+        
+        
+        return annotationView
     }
-    */
 
 }
