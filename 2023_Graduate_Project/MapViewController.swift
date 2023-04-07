155d989ae32d4ebf50f41d11741f39b555e2e1e5
DongHyunW0n <68365742+DongHyunW0n@users.noreply.github.com>
2023-04-08 02:00:45 +0900

    지도만드는중

--- 2023_Graduate_Project/MapViewController.swift
+++ 2023_Graduate_Project/MapViewController.swift
@@ -8,13 +8,22 @@
 import UIKit
 import MapKit
 import CoreLocation
+import Charts
+import Alamofire
+
 
 class MapViewController: UIViewController, MKMapViewDelegate {
     
     let map = MKMapView()
-    let coordinate = CLLocationCoordinate2D(
+    var coordinate1 = CLLocationCoordinate2D(
         latitude: 35.19716238064634,
         longitude: 129.06314400423676)
+    var coordinate2 = CLLocationCoordinate2D(
+        latitude: 35.19716238064634,
+        longitude: 129.08314500423676)
+    var coordinate3 = CLLocationCoordinate2D(
+        latitude: 35.27716238064634,
+        longitude: 129.08314500423676)
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,7 +32,7 @@ class MapViewController: UIViewController, MKMapViewDelegate {
         view.addSubview(map)
         map.frame = view.bounds
         
-        map.setRegion(MKCoordinateRegion(center: coordinate,
+        map.setRegion(MKCoordinateRegion(center: coordinate1,
                                          span: MKCoordinateSpan(
                                             latitudeDelta: 0.1,
                                             longitudeDelta: 0.1
@@ -32,17 +41,69 @@ class MapViewController: UIViewController, MKMapViewDelegate {
                       animated: false)
         
         map.delegate = self
-        addCustomPin()
+        addCustomPin1(title: "test3", subtitle: "test3", latitude: 35.196, longitude: 129.08)
+        addCustomPin2(title: "테스트 입니다", subtitle: "테스트 입니다당", latitude: 35.18, longitude: 129.1)
+        addCustomPin3(title: "테스트 입니다", subtitle: "테스트 입니다당", latitude: 35.18, longitude: 129.1)
+
         
     }
 
-    private func addCustomPin(){
+    private func setCoordinate (){
+        
+        
+        
+    }
+    
+    private func addCustomPin1( title : String, subtitle : String, latitude : Double, longitude : Double){
         
         let pin = MKPointAnnotation()
-        pin.coordinate = coordinate
-        pin.title = "우리집"
-        pin.subtitle = "사직자이"
+        
+        pin.coordinate = coordinate1
+        pin.title  = title
+        pin.subtitle = subtitle
         map.addAnnotation(pin)
+        let lat = latitude
+        let long = longitude
+        
+        let coordinate = CLLocationCoordinate2D(
+            latitude: lat,
+            longitude: long)
+        
+        
+    }
+    
+    private func addCustomPin2( title : String, subtitle : String, latitude : Double, longitude : Double){
+        
+        let pin = MKPointAnnotation()
+        
+        pin.coordinate = coordinate2
+        pin.title  = title
+        pin.subtitle = subtitle
+        map.addAnnotation(pin)
+        let lat = latitude
+        let long = longitude
+        
+        let coordinate = CLLocationCoordinate2D(
+            latitude: lat,
+            longitude: long)
+        
+        
+    }
+    
+    private func addCustomPin3( title : String, subtitle : String, latitude : Double, longitude : Double){
+        
+        let pin = MKPointAnnotation()
+        
+        pin.coordinate = coordinate3
+        pin.title  = title
+        pin.subtitle = subtitle
+        map.addAnnotation(pin)
+        let lat = latitude
+        let long = longitude
+        
+        let coordinate = CLLocationCoordinate2D(
+            latitude: lat,
+            longitude: long)
         
         
     }
