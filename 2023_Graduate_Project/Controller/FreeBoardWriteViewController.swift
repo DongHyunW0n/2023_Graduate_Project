1018fcac237f179eb8f186303e2d54038b627279
DongHyunW0n <68365742+DongHyunW0n@users.noreply.github.com>
2023-05-04 01:48:45 +0900

    수정

---
+++ 2023_Graduate_Project/Controller/FreeBoardWriteViewController.swift
@@ -0,0 +1,32 @@
+//
+//  FreeBoardWriteViewController.swift
+//  2023_Graduate_Project
+//
+//  Created by WonDongHyun on 2023/05/04.
+//
+
+import UIKit
+
+class FreeBoardWriteViewController: UIViewController {
+
+    @IBOutlet weak var TitleTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
