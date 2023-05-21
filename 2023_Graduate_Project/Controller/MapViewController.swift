272e6c7871bd217e70456de7b798a39da6a26661
DongHyunW0n <68365742+DongHyunW0n@users.noreply.github.com>
2023-05-22 02:04:35 +0900

    거의완성 !!

--- 2023_Graduate_Project/Controller/MapViewController.swift
+++ 2023_Graduate_Project/Controller/MapViewController.swift
@@ -36,9 +36,11 @@ class MapViewController : UIViewController, MKMapViewDelegate, CLLocationManager
         // 샘플 어노테이션 데이터
         let annotation1 = CustomAnnotation(title: "동서 철물", subtitle: "배관 전문점", coordinate: CLLocationCoordinate2D(latitude: 35.196, longitude: 129.08), rating: 4.5)
         let annotation2 = CustomAnnotation(title: "경남 철물", subtitle: "도배 전문점", coordinate: CLLocationCoordinate2D(latitude: 35.18, longitude: 129.1), rating: 4.8)
+        let annotation3 = CustomAnnotation(title: "부산 철물", subtitle: "도배 전문점", coordinate: CLLocationCoordinate2D(latitude: 35.156, longitude: 129.05), rating: 4.8)
+        let annotation4 = CustomAnnotation(title: "해운대 철물", subtitle: "도배 전문점", coordinate: CLLocationCoordinate2D(latitude: 35.15, longitude: 129.12), rating: 4.8)
         
         // 어노테이션을 MapView에 추가
-        mapView.addAnnotations([annotation1, annotation2])
+        mapView.addAnnotations([annotation1, annotation2 , annotation3, annotation4])
         
         // 초기 맵 뷰 영역 설정
         let centerCoordinate = coordinate3
@@ -83,7 +85,7 @@ class MapViewController : UIViewController, MKMapViewDelegate, CLLocationManager
         
         // 별점을 표시하는 라벨
         let ratingLabel = UILabel()
-        ratingLabel.text = "Rating: \(customAnnotation.rating)"
+        ratingLabel.text = "평점: \(customAnnotation.rating)"
         
         annotationView?.detailCalloutAccessoryView = ratingLabel
         
